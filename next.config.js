6e382ff253c335cf130acb55484366dafa270377
yonguk <lyw3059@gmail.com>
2024-07-06 11:09:17 +0900

    fix/next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // 설정 옵션 추가
+  };
+  
+  module.exports = nextConfig;
--- next.config.ts
+++
@@ -1,6 +0,0 @@
-import { NextConfig } from 'next';
-
-/** @type {NextConfig} */
-const nextConfig: NextConfig = {};
-
-export default nextConfig;
