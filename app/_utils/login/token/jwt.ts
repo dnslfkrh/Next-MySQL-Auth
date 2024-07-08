a986c70e5c6c6daad0e9d2a4058f67284f43e9e2
yongwuk <140808035+dnslfkrh@users.noreply.github.com>
2024-07-08 12:12:02 +0900

    feat/refreshTokenFunction

--- app/_utils/login/token/jwt.ts
+++ app/_utils/login/token/jwt.ts
@@ -1,21 +1,47 @@
 import jwt from 'jsonwebtoken';
 
-const createToken = async (id: string): Promise<string | null> => {
+const createAccessToken = async (id: string): Promise<string | null> => {
   try {
-    const payload = { id };
+    const payload = {
+      id: id,
+      type: 'access',
+      createdAt: new Date().toISOString()
+    };
     const SECRET_KEY = process.env.JWT_SECRET;
 
     if (!SECRET_KEY) {
       throw new Error('토큰 키 확인 필요');
     }
 
-    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
-    return token;
+    const accessToken = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+    return accessToken;
 
   } catch (error) {
-    console.error("Error creating token:", error);
+    console.error("Error creating access token:", error);
     return null;
   }
 }
 
-export default createToken;
+const createRefreshToken = async (id: string): Promise<string | null> => {
+  try {
+    const payload = {
+      id: id,
+      type: 'refresh',
+      createdAt: new Date().toISOString()
+    };
+    const SECRET_KEY = process.env.JWT_REFRESH_SECRET;
+
+    if (!SECRET_KEY) {
+      throw new Error('Refresh 토큰 키 확인 필요');
+    }
+
+    const refreshToken = jwt.sign(payload, SECRET_KEY, { expiresIn: '30d' });
+    return refreshToken;
+
+  } catch (error) {
+    console.error("Error creating refresh token:", error);
+    return null;
+  }
+}
+
+export { createAccessToken, createRefreshToken };
