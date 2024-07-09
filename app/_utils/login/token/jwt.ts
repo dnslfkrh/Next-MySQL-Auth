c2d351ec3c841ef17c59687e7f22624b6b68794f
yongwuk <140808035+dnslfkrh@users.noreply.github.com>
2024-07-09 11:46:53 +0900

    fix/jwt.ts

--- app/_utils/login/token/jwt.ts
+++ app/_utils/login/token/jwt.ts
@@ -22,26 +22,4 @@ const createAccessToken = async (id: string): Promise<string | null> => {
   }
 }
 
-const createRefreshToken = async (id: string): Promise<string | null> => {
-  try {
-    const payload = {
-      id: id,
-      type: 'refresh',
-      createdAt: new Date().toISOString()
-    };
-    const SECRET_KEY = process.env.JWT_REFRESH_SECRET;
-
-    if (!SECRET_KEY) {
-      throw new Error('토큰 키 확인 필요');
-    }
-
-    const refreshToken = jwt.sign(payload, SECRET_KEY, { expiresIn: '30d' });
-    return refreshToken;
-
-  } catch (error) {
-    console.error("Error creating refresh token:", error);
-    return null;
-  }
-}
-
-export { createAccessToken, createRefreshToken };
+export default createAccessToken;
