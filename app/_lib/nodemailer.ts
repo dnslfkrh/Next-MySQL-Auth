7b319ac73f8275b583db5f7f97f6175766bdc08b
yonguk <lyw3059@gmail.com>
2024-07-09 23:52:19 +0900

    fix/nodemailer

--- app/_lib/nodemailer.ts
+++ app/_lib/nodemailer.ts
@@ -3,56 +3,51 @@ import createLog from '../_utils/register/sendCode/saveLog';
 
 const sendCode = async (sendCase: number, email: string): Promise<boolean> => {
     try {
-        if (sendCase == 1) {
-            const code = await Math.random().toString(36).substring(2, 8);
-
-            const transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: process.env.EMAIL_USER,
-                    pass: process.env.EMAIL_KEY
-                }
-            });
-    
-            const mailOptions = {
-                to: email,
-                subject: 'Verification code',
-                text: `Your verification code is : [${code}]`
-            };
-
-            await transporter.sendMail(mailOptions);
-
-            const isLogCreate = await createLog(email, code);
-            if (!isLogCreate) {
-                return false;
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_KEY
             }
+        });
 
-            return true;
-        } 
-        
-        // ID 찾을 때
-        else if (sendCase == 2) {
-            // 이메일로 아이디 가져와야지
-            const transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: process.env.EMAIL_USER,
-                    pass: process.env.EMAIL_KEY
+        let mailOptions;
+        let code;
+
+        switch (sendCase) {
+            case 1: // 회원가입 인증번호
+                code = Math.random().toString(36).substring(2, 8);
+                mailOptions = {
+                    to: email,
+                    subject: 'Verification code',
+                    text: `Your verification code is : [${code}]`
+                };
+                await transporter.sendMail(mailOptions);
+                const isLogCreate = await createLog(email, code);
+                if (!isLogCreate) {
+                    return false;
                 }
-            });
-    
-            const mailOptions = {
-                to: email,
-                subject: 'Your ID',
-                text: `Your ID is : [${email}]`
-            };
 
-            await transporter.sendMail(mailOptions);
-        } 
-        
-        // 비밀번호 찾을 때 (case 3)
-        else {
-            // 비밀번호 바꿔오고 일단
+                break;
+
+            case 2: // ID 찾기
+                mailOptions = {
+                    to: email,
+                    subject: 'Your ID',
+                    text: `Your ID is : [${email}]`
+                };
+                await transporter.sendMail(mailOptions);
+
+                break;
+
+            case 3: // 비밀번호 찾기
+            
+            
+                break;
+
+            default:
+                console.error('Invalid sendCase');
+                return false;
         }
 
         return true;
